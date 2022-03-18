968d1049e4878fa283117e1654eb5510bd26aaf4
Andrew Hirschi <31867489+ashirsc@users.noreply.github.com>
2022-03-18 17:06:42 -0600

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: [
+      ".web.tsx",
+      ".web.ts",
+      ".tsx",
+      ".ts",
+      ".web.jsx",
+      ".web.js",
+      ".jsx",
+      ".js",
+      ".mjs",
+    ],
+  },
+  clearScreen: false,
+});
